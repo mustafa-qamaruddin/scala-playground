336ea301ff3eceb9ba613a2ee2b3ca979fc23b56
Mustafa Qamar-ud-Din <mustafa@mQuBits.com>
2020-09-23 08:34:26 +0200

    Scala Playground

---
+++ InteractWithJava.scala
@@ -0,0 +1,10 @@
+import java.util.{Date, Locale}
+import java.text.DateFormat._
+
+object FrenchDate {
+  def main(args: Array[String]): Unit = {
+    val now = new Date
+    val df = getDateInstance(LONG, Locale.FRANCE)
+    println(df format now)
+  }
+}
